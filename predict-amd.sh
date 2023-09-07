3abea6d239923090c5b7d55cafde22a021800666
Ales Krenek <ljocha@ics.muni.cz>
2023-09-07 14:30:00 +0200

    prepare for singularity/pbs

--- predict-amd.sh
+++ predict-amd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 image=ljocha/rassp
+version="$(cat VERSION)"
+sif=/storage/brno3-cerit/home/ljocha/singularity/rassp_${version}
 
 flags="-v ${PWD}:/work -w /work --rm -ti -e HOME=/work --ulimit nofile=32768:32768"
 user="-u $(id -u)"
-version="$(cat VERSION)"
 amdflags="--device=/dev/kfd --device=/dev/dri --shm-size 16G --group-add video --group-add render"
 nvidiaflags="--gpus=all"
 
@@ -13,6 +14,9 @@ if [ $(basename $0) = predict-amd.sh ]; then
 
 elif [ $(basename $0) = predict-nvidia.sh ]; then
 	docker run -ti ${flags} ${user} ${nvidiaflags} -p ${port}:${port} ${image}:nvidia-${version} python3 predict.py "$@"
+
+elif [ $(basename $0) = predict-singularity.sh ]; then
+	singularity exec --nv -B ${SCRATCHDIR}:/work --pwd /work python3 predict.py "$@"
 
 else
 	echo $0: WTF? >&2
---
+++ predict-singularity.sh
@@ -0,0 +1,1 @@
+predict-amd.sh
