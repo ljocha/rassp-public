3fe348ef40cbea98a69ef44991c61500260c7666
Ales Krenek <ljocha@ics.muni.cz>
2024-10-09 14:10:16 +0000

    workers & threads

--- run_rassp_train.py
+++ run_rassp_train.py
@@ -8,7 +8,7 @@ sys.path.insert(0,RASSP+'/rassp')
 
 import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
-# os.environ["OMP_NUM_THREADS"] = "12"
+os.environ["OMP_NUM_THREADS"] = "4"
 
 import torch
 
@@ -28,7 +28,7 @@ exp_config['checkpoint_every_n_epochs'] = 1
 exp_config['attempt_load_recent_checkpoint'] = True
 exp_config['validate_every'] = 10
 exp_config['cluster_config']['data_dir'] = '.'
-exp_config['DATALOADER_NUM_WORKERS'] = 8
+exp_config['DATALOADER_NUM_WORKERS'] = 12
 exp_config['exp_data']['data'][0]['db_filename'] = 'train-rassp-small.pq'
 exp_config['exp_data']['data'][1]['db_filename'] = 'valid-rassp-small.pq'
 
